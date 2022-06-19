bcd66b129e016542f48591d76540a5aaf6ccbcfb
maxkutovoy <maxkutovoy@yandex.ru>
2022-06-19 16:46:02 +0000

    Fix collectstatic step on auto-deploy

--- deploy_star_burger.sh
+++ deploy_star_burger.sh
@@ -17,7 +17,7 @@ pip  install --upgrade pip -r requirements.txt
 npm ci --dev
 ./node_modules/.bin/parcel build bundles-src/index.js --dist-dir bundles --public-url="./"
 
-echo yes | python manage.py collectstatic 
+python manage.py collectstatic --noinput
 python manage.py migrate
 
 sudo systemctl reload nginx
