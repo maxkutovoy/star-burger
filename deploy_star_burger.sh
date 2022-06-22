e87e06f9d958096434b14bb43c49bf4e12c06582
Max Kutovoy <maxkutovoy@yandex.ru>
2022-06-22 12:13:20 +0600

    Add noinput to migrate command in the deployment script.

--- deploy_star_burger.sh
+++ deploy_star_burger.sh
@@ -18,7 +18,7 @@ npm ci --dev
 ./node_modules/.bin/parcel build bundles-src/index.js --dist-dir bundles --public-url="./"
 
 python manage.py collectstatic --noinput
-python manage.py migrate
+python manage.py migrate --noinput
 
 sudo systemctl reload nginx
 sudo systemctl restart $gunicorn
