9888ad62e98c3a59d03cb88441c2168d74ec63a5
maxkutovoy <maxkutovoy@yandex.ru>
2022-07-09 08:57:14 +0000

    Del hardcode vars from certbot config

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
 
+set -e
+
+. ./.env
+
 if ! [ -x "$(command -v docker-compose)" ]; then
   echo 'Error: docker-compose is not installed.' >&2
   exit 1
 fi
 
-domains=(maxtestsite.ml www.maxtestsite.ml)
+domains=($DOMAIN_NAME www.$DOMAIN_NAME)
 rsa_key_size=4096
 data_path="./data/certbot"
-email="maxkutovoy@yandex.ru" # Adding a valid address is strongly recommended
+email=\""$EMAIL"\" # Adding a valid address is strongly recommended
 staging=0 # Set to 1 if you're testing your setup to avoid hitting request limits
 
 if [ -d "$data_path" ]; then
